224f3173f7519f3d8f2e7030f26197e16ccb323d
wendykarume <wendyjosephine.karume@strathmore.edu>
2018-01-10 17:41:32 +0300

    [Update] - Project

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Max wants Wendy so bad!!!!!");
+        System.out.println("Started project");
     }
     
 }
